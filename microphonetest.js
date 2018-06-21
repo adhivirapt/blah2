261cee13f9d472ea7c8c5a84b7970a65ad0b3006
Unknown <adhivira@live.com>
2018-06-22 01:38:37 +0200

    dsf

--- microphonetest.js
+++ microphonetest.js
@@ -1,25 +1,32 @@
 
 window.onload = function () {
-    navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
-        // store streaming data chunks in array
-        var counter = 0;
-        // create media recorder instance to initialize recording
-        const recorder = new MediaRecorder(stream);
-        // function to be called when data is received
-        recorder.ondataavailable = e => {
-            if (counter == 200) {
-                recorder.stop();
-                console.log("STOPME---" + new Date().getTime())
-            }
-            // add stream data to chunks
-            counter ++;
+    if (navigator.mediaDevices) {
+        navigator.mediaDevices.getUserMedia({ audio: true })
+        setTimeout(function () {
+            navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+                // store streaming data chunks in array
+                var counter = 0;
+                // create media recorder instance to initialize recording
+                const recorder = new MediaRecorder(stream);
+                // function to be called when data is received
+                recorder.ondataavailable = e => {
+                    if (counter == 200) {
+                        recorder.stop();
+                        console.log("STOPME---" + new Date().getTime())
+                    }
+                    // add stream data to chunks
+                    counter ++;
+        
+                };
+                // start recording with 1 second time between receiving 'ondataavailable' events
+                console.log("STARTME---" + new Date().getTime())
+                recorder.start(200)
+                // setTimeout to stop recording after 4 seconds
+        
+            }).catch(console.error);
+        }, 15000);
 
-        };
-        // start recording with 1 second time between receiving 'ondataavailable' events
-        console.log("STARTME---" + new Date().getTime())
-        recorder.start(200)
-        // setTimeout to stop recording after 4 seconds
+    }
 
-    }).catch(console.error);
 
 }
