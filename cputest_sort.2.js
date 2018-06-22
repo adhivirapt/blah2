42f2bd757568c3c9ca27724422bbf47aeaf89ff2
Unknown <adhivira@live.com>
2018-06-23 01:19:38 +0200

    hgd

--- cputest_sort.2.js
+++ cputest_sort.2.js
@@ -1,6 +1,8 @@
+window.onload = function () {
 
-window.onload = function () { 
-    console.log("STARTME---" + new Date().getTime())
-    array200000.sort();
-    console.log("STOPME---" + new Date().getTime())
+    setTimeout(function () {
+        console.log("STARTME---" + new Date().getTime())
+        array200000.sort();
+        console.log("STOPME---" + new Date().getTime())
+    }, 15000);
 }
