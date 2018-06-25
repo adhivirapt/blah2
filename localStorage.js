c01a53f2aa2a919175ede1aeda2d8f39e3b6f1d1
Unknown <adhivira@live.com>
2018-06-25 17:12:22 +0200

    sdf

---
+++ localStorage.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    console.log("STARTME---" + new Date().getTime())
+    for (var i = 0; i < 125000; i++) {
+        localStorage.setItem(i, "");
+        // console.log(localStorage.getItem(i));
+    }
+    console.log("STOPME---" + new Date().getTime())
+
+
+}
